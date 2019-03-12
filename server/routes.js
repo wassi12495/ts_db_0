4d8bc0f743477171c623812523dc7390db5734b4
Josh Wasserman <joshbwasserman@gmail.com>
2019-03-12 18:30:35 -0400

    update

--- server/routes.js
+++ server/routes.js
@@ -11,11 +11,11 @@ router.get('/', (req, res) => {
   res.send(people);
 });
 
-router.post('/createPerson', (req, res) => {
-  console.log(req.body);
-  people.push(req.body);
-  res.send(req.body);
-});
+// router.post('/createPerson', (req, res) => {
+//   console.log(req.body);
+//   people.push(req.body);
+//   res.send(req.body);
+// });
 
 // Testing schema
 var Ajv = require('ajv');
@@ -27,15 +27,22 @@ let data = {
   firstName: 'Josh',
   lastName: 'wasserman',
   birthday: '2999-09-17',
-  email: 'email@address',
+  email: 'email@address.com',
   phone: '7777777777',
 };
 
-router.post('/test', (req, res) => {
+// "pattern": "^[a-zA-Z0-9._%+-]+@[a-z0-9]+.[a-z]{3}$"
+
+router.post('/createPerson', (req, res) => {
   // console.log(validate);
-  var valid = validate(data);
+  console.log(req.body);
+  var valid = validate(req.body);
+
   console.log(valid);
   if (!valid) console.log(validate.errors);
+  else {
+    people.push(req.body);
+  }
   res.send(valid);
 });
 module.exports = {
