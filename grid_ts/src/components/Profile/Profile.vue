ba11224627e4c85a260dd4fe007132d001c7b29a
Josh Wasserman <joshbwasserman@gmail.com>
2019-03-27 09:26:18 -0400

    EOD

--- grid_ts/src/components/Profile/Profile.vue
+++ grid_ts/src/components/Profile/Profile.vue
@@ -3,15 +3,17 @@
     <h1>
       Profile
     </h1>
+
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-import { Action } from "vuex-class";
-
+import { Action, Getter } from "vuex-class";
+@Component({})
 export default class Profile extends Vue {
+  @Getter("userSelected") userSelected;
   mounted() {
     console.log("Mounted  Profile Page");
   }
