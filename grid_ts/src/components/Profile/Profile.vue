c25ac714cbc3930051fc8d7e2f3addb44bbf175f
Josh Wasserman <github email address>
2019-04-01 11:00:55 -0400

    profile

--- grid_ts/src/components/Profile/Profile.vue
+++ grid_ts/src/components/Profile/Profile.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <h1>
-      Profile
-    </h1>
-
+    <h1>Profile</h1>
+    <button>Edit</button>
+    <br>
+    <h2>Name: {{this.fullName}}</h2>
+    <h2>Phone: {{this.phone}}</h2>
+    <h2>Email: {{this.email}}</h2>
+    <h2>Birthday: {{this.birthday}}</h2>
   </div>
 </template>
 
@@ -13,9 +16,26 @@ import Component from "vue-class-component";
 import { Action, Getter } from "vuex-class";
 @Component({})
 export default class Profile extends Vue {
+  data() {
+    return {
+      fullName: null,
+      birthday: null,
+      phone: null,
+      email: null,
+      id: null
+    };
+  }
+
   @Getter("userSelected") userSelected;
   mounted() {
-    console.log("Mounted  Profile Page");
+    console.log("Mounted  Profile Page", this.userSelected);
+
+    this.fullName = `${this.userSelected.first}` + ` ${this.userSelected.last}`;
+    this.birthday = this.userSelected.birthday;
+    this.phone = this.userSelected.phone;
+    this.email = this.userSelected.email;
+    this.id = this.userSelected.id;
+    console.log("data", this._data);
   }
 }
 </script>
